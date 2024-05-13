ef717962639b3f9c30983aecb9b7261467d8f5c0
RenatoAugusto17 <169068426+RenatoAugusto17@users.noreply.github.com>
2024-05-13 21:49:54 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("    Curso Técnico:");
+    System.out.println("   Desenvolvimento de Programas");
+    System.out.println("");
+    System.out.println("   Bem, aqui estarei relantando um pouco do meu entender até agora do curso técnico de Desenvolvimento de Projetos.");
+    System.out.println("   Em Lógica Computacional nós estudantes tivemos como objetivo entender um pouco sobre o básico das preposições e linguagem de programação.");
+    System.out.println("   Já nas outras matérias como Ciência da Computação e Análise e Projeto de Sistemas nós aprendemos um pouco sobre alguns termos gerais da área da computação, como por exemplo oque é um hardware e oque é um software, no qual aprendemos que hardware é a parte do computador que conseguimos tocar fisícamente, já software é aquele que não conseguimos tocar no sentido fisíco, como servidores, um exemplo pode ser o google.");
+    System.out.println("   Resumidamente então em Lógica Computacional foi mais sobre línguagem dos computadores, e em Ciência Computacional e Análise e Projeto de Sistemas foi sobre um geral do básico da área dos computadores");
+  }
+}
